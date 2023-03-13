cb50895c22b8ad727690efd6ef71466924b52e69
Colin <colinb71037@hotmail.com>
2023-03-13 19:48:50 -0400

    Colin: Pushed my Initial Changes

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public Vector2 dir;
+
+    [SerializeField] // what is a serializedfield
+    public float speed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.CompareTag("PaddleRight"))
+        {
+
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 0, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.CompareTag("PaddleRight"))
+        {
+            GetComponent<SpriteRenderer>().color = new Color(0, 0, Mathf.Abs(Mathf.Sin(Time.time)));
+            // Movement 
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(-Vector3.up * speed * Time.deltaTime);
+            }
+        }
+        else if (transform.CompareTag("PaddleLeft"))
+        {
+            GetComponent<SpriteRenderer>().color = new Color(Mathf.Abs(Mathf.Sin(Time.time)), 0, Mathf.Abs(Mathf.Sin(Time.time)));
+            // Movement
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-Vector3.up * speed * Time.deltaTime);
+            }
+        }
+    }
+    
+}
